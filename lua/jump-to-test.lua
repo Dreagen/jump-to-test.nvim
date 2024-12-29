25011beb08e67227bd3a926ec1d0247fe3521f4c
Nick Allen <nick.allen@dataglide.co>
2024-12-29 14:18:23 +0000

    Extract scan dir function

--- lua/jump-to-test.lua
+++ lua/jump-to-test.lua
@@ -1,5 +1,29 @@
 local M = {}
 
+local function scan_dir(dir, search)
+	local handle = vim.loop.fs_scandir(dir)
+	if not handle then
+		return nil
+	end
+
+	while true do
+		local name, type = vim.loop.fs_scandir_next(handle)
+		if not name then
+			break
+		end
+
+		local full_path = dir .. "/" .. name
+		if type == "file" and name == search then
+			return full_path
+		elseif type == "directory" then
+			local result = scan_dir(full_path, search)
+			if result then
+				return result
+			end
+		end
+	end
+end
+
 function M.jump_to_test()
 	local root_dir = vim.fn.getcwd()
 
@@ -8,31 +32,7 @@ function M.jump_to_test()
 	local filename = vim.fn.fnamemodify(filepath, ":t")
 	local test_filename = vim.fn.fnamemodify(filename, ":r") .. "Tests." .. file_ext
 
-	local function scan_dir(dir)
-		local handle = vim.loop.fs_scandir(dir)
-		if not handle then
-			return nil
-		end
-
-		while true do
-			local name, type = vim.loop.fs_scandir_next(handle)
-			if not name then
-				break
-			end
-
-			local full_path = dir .. "/" .. name
-			if type == "file" and name == test_filename then
-				return full_path
-			elseif type == "directory" then
-				local result = scan_dir(full_path)
-				if result then
-					return result
-				end
-			end
-		end
-	end
-
-	local test_file_path = scan_dir(root_dir)
+	local test_file_path = scan_dir(root_dir, test_filename)
 
 	if not test_file_path then
 		print("could not find test file: ", test_filename)
@@ -48,31 +48,7 @@ function M.jump_to_source()
 	local filename = vim.fn.fnamemodify(filepath, ":t")
 	local source_filename = filename:gsub("Tests", "")
 
-	local function scan_dir(dir)
-		local handle = vim.loop.fs_scandir(dir)
-		if not handle then
-			return nil
-		end
-
-		while true do
-			local name, type = vim.loop.fs_scandir_next(handle)
-			if not name then
-				break
-			end
-
-			local full_path = dir .. "/" .. name
-			if type == "file" and name == source_filename then
-				return full_path
-			elseif type == "directory" then
-				local result = scan_dir(full_path)
-				if result then
-					return result
-				end
-			end
-		end
-	end
-
-	local source_file_path = scan_dir(root_dir)
+	local source_file_path = scan_dir(root_dir, source_filename)
 
 	if not source_file_path then
 		print("could not find source file: ", source_filename)
